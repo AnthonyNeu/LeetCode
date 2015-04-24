0082d3025f071659c449d12ce21d14673a9c1607
AnthonyNeu <anthonyjin0619@gmail.com>
2015-04-23 20:04:10 -0700

    finish 2 solutions

---
+++ Python/Remove Linked List Elements.py
@@ -0,0 +1,28 @@
+"""
+Remove all elements from a linked list of integers that have value val.
+
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @param {integer} val
+    # @return {ListNode}
+    def removeElements(self, head, val):
+        dummy = ListNode(0)
+        dummy.next,prev,cur = head,dummy,head
+        while cur:
+            if cur.val == val:
+                prev.next = cur.next
+            else:
+                prev = cur
+            cur = cur.next
+        return dummy.next

5c13bfe99dc05e5a4f198d16495a77ad989b226b
AnthonyNeu <anthonyjin0619@gmail.com>
2015-06-12 13:30:17 -0700

    finish 1 solution

---
+++ Python/Invert Binary Tree.py
@@ -0,0 +1,33 @@
+"""
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {TreeNode}
+    def invertTree(self, root):
+        if root is None: return root
+        root.left,root.right= self.invertTree(root.right),self.invertTree(root.left)
+        return root

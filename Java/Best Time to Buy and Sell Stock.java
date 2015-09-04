9552d8421bf53b1acb0071fba9dc342ef6fec9e2
Anthony <anthonyjin0619@gmail.com>
2015-09-03 22:14:59 -0700

    finish 1 solution

---
+++ Java/Best Time to Buy and Sell Stock.java
@@ -0,0 +1,22 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+*/
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;  //just remember the smallest price
+        int profit = 0;
+        for (int i : prices) {
+            min = i < min ? i : min;
+            profit = (i - min) > profit ? i - min : profit;
+        }
+
+        return profit;
+    }
+}

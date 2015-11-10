f5bb9373f59edcdd28dd2dbdf6b63a49e379259c
Anthony <anthonyjin0619@gmail.com>
2015-11-09 21:31:02 -0800

    add Range Sum Query - Immutable

---
+++ Python/Range Sum Query - Immutable.py
@@ -0,0 +1,40 @@
+"""
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+"""
+
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.sums = []
+        cur_sum = 0
+        for num in nums:
+            cur_sum += num
+            self.sums.append(cur_sum)
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.sums[j] - self.sums[i - 1] if i else self.sums[j]
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)

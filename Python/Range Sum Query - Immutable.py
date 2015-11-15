6226afe222877229758982dd43e077aa8e68e0a1
Anthony <anthonyjin0619@gmail.com>
2015-11-14 16:24:44 -0800

    add segment tree solution for Range Sum Query

--- Python/Range Sum Query - Immutable.py
+++ Python/Range Sum Query - Immutable.py
@@ -33,7 +33,59 @@ class NumArray(object):
         """
         return self.sums[j] - self.sums[i - 1] if i else self.sums[j]
 
+class SegmentTreeNode(object):
+    def __init__(self, start, end):
+        self.start, self.end, self.sum = start, end, None
+        self.left, self.right = None, None
 
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.segment_tree_root = self.build(0, len(nums) - 1, nums)
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.query(self.segment_tree_root, i, j)
+
+    def build(self, start, end, nums):
+        if start > end:
+            return None
+        root = SegmentTreeNode(start, end)
+        if start != end:
+            mid = start + (end - start) / 2
+            root.left = self.build(start, mid, nums)
+            root.right = self.build(mid + 1, end, nums)
+            root.sum = root.left.sum + root.right.sum
+        else:
+            root.sum = nums[start]
+        return root
+
+    def query(self, root, start, end):
+        if not root:
+            return 0
+        if root.start >= start and root.end <= end:
+            return root.sum
+        mid = root.start + (root.end - root.start) / 2
+        left_sum, right_sum = 0, 0
+        if start <= mid:
+            if end > mid:
+                left_sum = self.query(root.left, start, mid)
+            else:
+                left_sum = self.query(root.left, start, end)
+        if end > mid:
+            if start <= mid:
+                right_sum = self.query(root.right, mid + 1, end)
+            else:
+                right_sum = self.query(root.right, start, end)
+        return left_sum + right_sum
 # Your NumArray object will be instantiated and called as such:
 # numArray = NumArray(nums)
 # numArray.sumRange(0, 1)

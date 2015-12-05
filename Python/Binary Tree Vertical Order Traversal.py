94c28cbebea8edf23f1f505e5b71ca9233c3bcef
Anthony <anthonyjin0619@gmail.com>
2015-12-05 10:34:19 -0800

    add Binary Tree Vertical Order Traversal

---
+++ Python/Binary Tree Vertical Order Traversal.py
@@ -0,0 +1,56 @@
+"""
+Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+
+If two nodes are in the same row and column, the order should be from left to right.
+
+Examples:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its vertical order traversal as:
+[
+  [9],
+  [3,15],
+  [20],
+  [7]
+]
+Given binary tree [3,9,20,4,5,2,7],
+    _3_
+   /   \
+  9    20
+ / \   / \
+4   5 2   7
+return its vertical order traversal as:
+[
+  [4],
+  [9],
+  [3,5,2],
+  [20],
+  [7]
+]
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def verticalOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        queue, order = collections.deque([(root, 0)]), collections.defaultdict(list)
+        while queue:
+            node, idx = queue.popleft()
+            if node:
+                order[idx].append(node.val)
+                queue.append((node.left, idx - 1))
+                queue.append((node.right, idx + 1))
+        return [order[key] for key in sorted(order)]

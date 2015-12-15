f769de6320ad0dab034607d442239006f8fddf2a
Anthony <anthonyjin0619@gmail.com>
2015-12-14 23:35:58 -0800

    update Basic Calculator II

--- Python/4Sum.py
+++ Python/4Sum.py
@@ -13,35 +13,35 @@ The solution set must not contain duplicate quadruplets.
 
 class Solution:
     # @return a list of lists of length 4, [[val1,val2,val3,val4]]
-    def fourSum(self, num, target):
+    def fourSum(self, nums, target):
         result = []
-        num = sorted(num)
-        for i in range(len(num)):
-            if i > 0 and num[i] == num[i-1]:
+        nums = sorted(nums)
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            for j in range(i+1,len(num)):
-                if j > i+1 and num[j] == num[j-1]:
+            for j in range(i+1,len(nums)):
+                if j > i+1 and nums[j] == nums[j-1]:
                     continue
-                x = target - num[i] - num[j]
+                x = target - nums[i] - nums[j]
                 left = j+1
-                right = len(num)-1
+                right = len(nums)-1
                 while left < right:
-                    if left > j + 1 and num[left] == num[left-1]:
+                    if left > j + 1 and nums[left] == nums[left-1]:
                         left +=1
                         continue
-                    if right < len(num) -1 and num[right] == num[right+1]:
+                    if right < len(nums) -1 and nums[right] == nums[right+1]:
                         right -=1
                         continue
-                    if num[left] + num[right] == x:
+                    if nums[left] + nums[right] == x:
                         tmp = []
-                        tmp.append(num[i])
-                        tmp.append(num[j])
-                        tmp.append(num[left])
-                        tmp.append(num[right])
+                        tmp.append(nums[i])
+                        tmp.append(nums[j])
+                        tmp.append(nums[left])
+                        tmp.append(nums[right])
                         result.append(tmp)
                         left +=1
                         right-=1
-                    elif num[left] + num[right] < x:
+                    elif nums[left] + nums[right] < x:
                         left += 1
                     else:
                         right -=1
--- Python/Basic Calculator II.py
+++ Python/Basic Calculator II.py
@@ -53,3 +53,56 @@ class Solution:
             operands.append(left * right)
         elif op == '/':
             operands.append(left / right)
+
+# use expression tree 
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ops, result, number = [], [], ""
+        def _calculate(left, right, op):
+            i0, i1 = int(left), int(right)
+            if op == "+":
+                return str(i0 + i1)
+            elif op == "-":
+                return str(i0 - i1)
+            elif op == "*":
+                return str(i0 * i1)
+            elif op == "/":
+                return str(i0 / i1)
+        for string in s:
+            if string is " ":
+                continue
+            elif string.isdigit():
+                number += string
+            else:
+                if number:
+                    result.append(number)
+                    number = ""
+                if string == '-' or string == '+':
+                    while ops:
+                        op = ops.pop()
+                        right = result.pop()
+                        left = result.pop()
+                        result.append(_calculate(left, right, op))
+                    ops.append(string)
+                elif string == '*' or string == '/':
+                    while ops and ops[-1] not in ('+', '-'):
+                        op = ops.pop()
+                        right = result.pop()
+                        left = result.pop()
+                        result.append(_calculate(left, right, op))
+                    ops.append(string)
+        if number:
+            result.append(number)
+        while ops:
+            op = ops.pop()
+            right = result.pop()
+            left = result.pop()
+            result.append(_calculate(left, right, op))
+        if not result:
+            return 0
+        else:
+            return int(result[-1])
